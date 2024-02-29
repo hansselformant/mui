d6ade729750517af10f1ee25ad2359ef2672e166
hansselformant <hanssel@formant.io>
2024-02-29 16:57:40 -0500

    adding theme logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// import Button from '@mui/material/Button'
+import { Button, Typography } from "@mui/material";
+import { Settings, Add } from "@mui/icons-material";
+import styled from "@emotion/styled";
+// import SettingsIcon from '@mui/icons-material/Settings';
 
 function App() {
+  //using emotion components with styled
+  const BlueButton = styled(Button)({
+    backgroundColor: "skyblue",
+    color: "#888",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue",
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white",
+    },
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="text" startIcon={<Settings />}>
+        Settings
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => {
+          alert("clicked");
+        }}
+      >
+        Contained
+      </Button>
+      <Button
+        variant="contained"
+        startIcon={<Add />}
+        color="success"
+        onClick={() => {
+          alert("clicked");
+        }}
+      >
+        Add
+      </Button>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
+      <Typography variant="h1" component="p">
+        h1. Heading in p element
+      </Typography>
+      <BlueButton> 1st</BlueButton>
+      <BlueButton> 2nd</BlueButton>
     </div>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material"
+
+export const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+          main: '#74b53f',
+        },
+        secondary: {
+          main: '#f50057',
+        },
+    },
+})
